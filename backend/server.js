061533cffecc5dfd1daa05bc16dc03e26deffed9
dmytropk <dmytro18wa@gmail.com>
2020-02-28 11:21:09 -0800

    mongoDB database connection established

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 //create an Express server, attach the cors and express.json middleware
 const express = require('express'); 
 const cors = require('cors');
+const mongoose = require('mongoose');  /* to interact with mongoDB through node.js simpler */
 
 //to store configuration in the environment separate from code
 require('dotenv').config();
@@ -10,6 +11,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+//.env file for connection string taken from MongoDB Atlas
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("mongoDB database connection established successfully");
+})
 
 //make the server listen on port 5000
 app.listen(port, () => {
